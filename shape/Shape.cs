d4d8c10450e71c7e7ea3dbe298f62f5685b9bb2d
bencsii <benicke001@gmail.com>
2025-03-27 16:48:52 +0100

    Shape osztaly kesz

---
+++ shape/Shape.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    internal abstract class Shape
+    {
+        bool isHoley;
+        string color;
+
+        public string Color { get => color; set => color = value; }
+
+        protected Shape(string color, bool isHoley = false)
+        {
+            this.isHoley = isHoley;
+            this.color = color;
+        }
+
+        protected Shape(string color)
+        {
+            this.color = color;
+            isHoley = false;
+        }
+
+        public void MakeHoley()
+        {
+            isHoley = true;
+        }
+
+
+        public abstract double Perimeter();
+        public abstract double Area();
+
+
+        public override string? ToString()
+        {
+            return $"Color: {color}\nIsHoley: {isHoley}\n\t-Perimeter: {Perimeter()}\n\t-Area: {Area()}";
+        }
+    }
+}
