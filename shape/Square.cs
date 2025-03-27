2049769ac77dc4db7e6975c40035d202abce8df8
bencsii <benicke001@gmail.com>
2025-03-27 17:09:54 +0100

    Square osztaly kesz

---
+++ shape/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    internal class Square : Rectangle
+    {
+        public double Height { get => Height; set => Height = value; }
+        public double Width { get => Width; set => Width = value; }
+        public Square(bool isHoley, string color, double height, double width) : base(isHoley, color, height, width)
+        {
+        }
+
+        public override string? ToString()
+        {
+            string toString = base.ToString();
+            return toString.Remove(toString.Length - ("Rectangle".ToCharArray().Length)) + "Square";
+        }
+    }
+}
